4e4a3552acc6fc070a5ed1d7ce8b6bdc17e7f094
Shaw Malcolm <shawmalcolm@protonmail.com>
2021-03-17 07:35:48 +0000

    add content to markdown

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,39 +1,40 @@
 module.exports = {
-  purge: ['./pages/**/*.{js,ts,jsx,tsx}', './components/**/*.{js,ts,jsx,tsx}'],
-  darkMode: 'class',
+  purge: ["./pages/**/*.{js,ts,jsx,tsx}", "./components/**/*.{js,ts,jsx,tsx}"],
+  darkMode: "class",
   theme: {
     extend: {
       gridTemplateRows: {
-        'x': 'auto 1fr auto',
-        'projectRows': 'repeat(3, 500px)'
+        x: "auto 1fr auto",
+        projectRows: "repeat(3, 500px)",
       },
       gridTemplateColumns: {
-        'projectColumns': 'repeat(auto-fit, minmax(100px, 1fr))'
-      }
+        projectColumns: "repeat(auto-fit, minmax(100px, 1fr))",
+      },
     },
     screens: {
-      'sm': '280px',
-      'md': '768px',
-      'lg': '1024px',
-      'xl': "1280px",
-      'xxl': "1536px"
+      sm: "280px",
+      md: "768px",
+      lg: "1024px",
+      xl: "1280px",
+      xxl: "1536px",
     },
     fontFamily: {
-      'sans': ['Inter']
+      sans: ["Inter"],
     },
     colors: {
-      'orange': '#d1600c',
-      'charcoal': '#303030',
-      'chocolate': '#1d1f21',
-      'cream': '#c9cacc',
-      'black': '#000'
+      orange: "#d1600c",
+      charcoal: "#303030",
+      chocolate: "#1d1f21",
+      cream: "#c9cacc",
+      black: "#000",
+      mint: "#2bbc8a",
     },
     minWidth: {
-      '50': '50%',
-    }
+      50: "50%",
+    },
   },
   variants: {
     extend: {},
   },
   plugins: [],
-}
+};
