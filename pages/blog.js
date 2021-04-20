9879e32bf71c9ffad52a6f06cba46cd11d42f94b
Shaw Malcolm <shawmalcolm@protonmail.com>
2021-04-20 17:32:46 +0100

    add content-security-policy

--- pages/blog.js
+++ pages/blog.js
@@ -1,67 +1,79 @@
-import Link from "next/link";
-import fs from "fs";
-import matter from "gray-matter";
-import Layout from "../components/layout";
+import Link from 'next/link'
+import fs from 'fs'
+import matter from 'gray-matter'
+import Layout from '../components/layout'
 
 export default function Blog({ posts }) {
-  const Posts = posts.sort((a, b) => b.frontmatter.id - a.frontmatter.id);
+  const Posts = posts.sort((a, b) => b.frontmatter.id - a.frontmatter.id)
   return (
-    <Layout>
-     
-      <div className="m:mx-4 sm:mx-6 md:mx-16 max-w-screen-md lg:mx-48 xl:mx-96 lg:mb-48">
-      <h1 className="text-pumpkin text-3xl mb-1 text-center">Blog List</h1>
-      <h2 className="mb-4 text-black dark:text-white text-center">Click on a title for full post</h2>
-        {Posts.map(({ frontmatter: { title, description, date }, slug }) => (
-          <article key={slug} className="">
-            <header>
-              <h3 className="mb-1 text-base">
-                <Link href={"/post/[slug]"} as={`/post/${slug}`}>
-                  <a className="text-2xl text-pumpkin anchor hover:text-blue dark:hover:text-green">
-                    {title}
-                  </a>
-                </Link>
-              </h3>
-              <p className="text-sm mb-1 text-pumpkin dark:text-cream">
-                {date}
-              </p>
-            </header>
-            <section>
-              <p className="mb-8 text-black dark:text-cream">{description}</p>
-            </section>
-          </article>
-        ))}
-      </div>
-    </Layout>
-  );
+    <>
+      <Head>
+        <meta
+          http-equiv='Content-Security-Policy'
+          content="connect-src 'self'; default-src 'none; img-src 'self; manifest-src 'self'; script-src-elem 'self'; style-src-elem 'self';"
+        />
+        <title>Shaw Malcolm - Developer and Tester</title>
+        <link rel='icon' href='/favicon.ico' />
+        <link href='public/fonts/style.css' rel='stylesheet' />
+      </Head>
+      <Layout>
+        <div className='m:mx-4 sm:mx-6 md:mx-16 max-w-screen-md lg:mx-48 xl:mx-96 lg:mb-48'>
+          <h1 className='text-pumpkin text-3xl mb-1 text-center'>Blog List</h1>
+          <h2 className='mb-4 text-black dark:text-white text-center'>
+            Click on a title for full post
+          </h2>
+          {Posts.map(({ frontmatter: { title, description, date }, slug }) => (
+            <article key={slug} className=''>
+              <header>
+                <h3 className='mb-1 text-base'>
+                  <Link href={'/post/[slug]'} as={`/post/${slug}`}>
+                    <a className='text-2xl text-pumpkin anchor hover:text-blue dark:hover:text-green'>
+                      {title}
+                    </a>
+                  </Link>
+                </h3>
+                <p className='text-sm mb-1 text-pumpkin dark:text-cream'>
+                  {date}
+                </p>
+              </header>
+              <section>
+                <p className='mb-8 text-black dark:text-cream'>{description}</p>
+              </section>
+            </article>
+          ))}
+        </div>
+      </Layout>
+    </>
+  )
 }
 
 export async function getStaticProps() {
-  const files = fs.readdirSync(`${process.cwd()}/content/posts`);
+  const files = fs.readdirSync(`${process.cwd()}/content/posts`)
 
   const posts = files.map((filename) => {
     const markdownWithMetaData = fs
       .readFileSync(`content/posts/${filename}`)
-      .toString();
+      .toString()
 
-    const { data } = matter(markdownWithMetaData);
+    const { data } = matter(markdownWithMetaData)
 
-    const options = { day: "numeric", month: "long", year: "numeric" };
-    const formattedDate = data.date.toLocaleDateString("en-GB", options);
+    const options = { day: 'numeric', month: 'long', year: 'numeric' }
+    const formattedDate = data.date.toLocaleDateString('en-GB', options)
 
     const frontmatter = {
       ...data,
       date: formattedDate,
-    };
+    }
 
     return {
-      slug: filename.replace(".md", ""),
+      slug: filename.replace('.md', ''),
       frontmatter,
-    };
-  });
+    }
+  })
 
   return {
     props: {
       posts,
     },
-  };
+  }
 }
--- pages/projects.js
+++ pages/projects.js
@@ -2,76 +2,98 @@ import Link from 'next/link'
 import fs from 'fs'
 import matter from 'gray-matter'
 import Layout from '../components/layout'
-import Image from "next/image";
+import Image from 'next/image'
 
 export default function Projects({ projects }) {
   const Projects = projects.sort((a, b) => b.frontmatter.id - a.frontmatter.id)
   return (
-    <Layout>
-      {' '}
-      <div className='flex flex-wrap overflow-hidden sm:mx-3 md:mx-2 lg:mx-2 xl:mx-3 mb-36'>
-        {Projects.map(
-          ({
-            frontmatter: {
-              title,
-              description,
-              image,
-              alt,
-              type,
-              tools,
-              source,
-              visit,
-            },
-            slug,
-          }) => (
-            <article
-              key={slug}
-              className='rounded-lg  p-2 lg:mx-12 w-full overflow-hidden sm:my-3 sm:px-3 sm:w-full md:my-2 md:px-2 md:w-1/2 lg:my-2 lg:px-2 lg:w-1/3 xl:my-3 xl:px-3 xl:w-1/4'
-            >
-              <header>
-                <h2 className='mb'>
-                  <Link href={'/project/[slug]'} as={`/project/${slug}`}>
-                    <a className='text-base anchor hover:text-blue dark:hover:text-green '>{title}</a>
-                  </Link>
-                </h2>
-                <p className='text-sm mb-1 text-black dark:text-white'>{description}</p>
-              </header>
-              <section className='flex justify-center mt-2 mb-2'>
-              <Image
-              src={image}
-              alt={alt}
-              width={500}
-              height={375}
-            />
-              </section>
-              <section>
-                <p className='text-sm mb-1 text-pumpkin'>
-                  Type: <span className='text-black dark:text-white'>{type}</span>
-                </p>
-                <p className='text-sm mb-1 text-pumpkin'>
-                  Built with:{' '}
-                  {tools.map((tool, index) => (
-                    <span key={index} className='text-black dark:text-white'>
-                      {index === tools.length - 1 ? `${tool} ` : `${tool}, `}
-                    </span>
-                  ))}
-                </p>
-                <p className='text-sm mb-1'>
-                  <a href={source} target='_blank' rel='noopener noreferer' className='anchor hover:text-blue dark:hover:text-green'>
-                    Source Code
-                  </a>
-                </p>
-                <p className='text-sm mb-1'>
-                  <a href={visit} target='_blank' rel='noopener noreferer' className='anchor hover:text-blue dark:hover:text-green'>
-                    Visit
-                  </a>
-                </p>
-              </section>
-            </article>
-          )
-        )}
-      </div>
-    </Layout>
+    <>
+      <Head>
+        <meta
+          http-equiv='Content-Security-Policy'
+          content="connect-src 'self'; default-src 'none; img-src 'self; manifest-src 'self'; script-src-elem 'self'; style-src-elem 'self';"
+        />
+        <title>Shaw Malcolm - Developer and Tester</title>
+        <link rel='icon' href='/favicon.ico' />
+        <link href='public/fonts/style.css' rel='stylesheet' />
+      </Head>
+
+      <Layout>
+        {' '}
+        <div className='flex flex-wrap overflow-hidden sm:mx-3 md:mx-2 lg:mx-2 xl:mx-3 mb-36'>
+          {Projects.map(
+            ({
+              frontmatter: {
+                title,
+                description,
+                image,
+                alt,
+                type,
+                tools,
+                source,
+                visit,
+              },
+              slug,
+            }) => (
+              <article
+                key={slug}
+                className='rounded-lg  p-2 lg:mx-12 w-full overflow-hidden sm:my-3 sm:px-3 sm:w-full md:my-2 md:px-2 md:w-1/2 lg:my-2 lg:px-2 lg:w-1/3 xl:my-3 xl:px-3 xl:w-1/4'
+              >
+                <header>
+                  <h2 className='mb'>
+                    <Link href={'/project/[slug]'} as={`/project/${slug}`}>
+                      <a className='text-base anchor hover:text-blue dark:hover:text-green '>
+                        {title}
+                      </a>
+                    </Link>
+                  </h2>
+                  <p className='text-sm mb-1 text-black dark:text-white'>
+                    {description}
+                  </p>
+                </header>
+                <section className='flex justify-center mt-2 mb-2'>
+                  <Image src={image} alt={alt} width={500} height={375} />
+                </section>
+                <section>
+                  <p className='text-sm mb-1 text-pumpkin'>
+                    Type:{' '}
+                    <span className='text-black dark:text-white'>{type}</span>
+                  </p>
+                  <p className='text-sm mb-1 text-pumpkin'>
+                    Built with:{' '}
+                    {tools.map((tool, index) => (
+                      <span key={index} className='text-black dark:text-white'>
+                        {index === tools.length - 1 ? `${tool} ` : `${tool}, `}
+                      </span>
+                    ))}
+                  </p>
+                  <p className='text-sm mb-1'>
+                    <a
+                      href={source}
+                      target='_blank'
+                      rel='noopener noreferer'
+                      className='anchor hover:text-blue dark:hover:text-green'
+                    >
+                      Source Code
+                    </a>
+                  </p>
+                  <p className='text-sm mb-1'>
+                    <a
+                      href={visit}
+                      target='_blank'
+                      rel='noopener noreferer'
+                      className='anchor hover:text-blue dark:hover:text-green'
+                    >
+                      Visit
+                    </a>
+                  </p>
+                </section>
+              </article>
+            )
+          )}
+        </div>
+      </Layout>
+    </>
   )
 }
 
